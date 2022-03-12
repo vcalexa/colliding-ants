8100436c82accf31d2482d5ee4b16965bee5e7c8
W <gg>
2022-03-12 19:29:28 +0100

    improvement

--- src/main/kotlin/Ant.kt
+++ src/main/kotlin/Ant.kt
@@ -6,7 +6,6 @@ var time = 0
 var collisions = 0
 
 var outputString = Array(100) { _ -> "." }
-var temp = outputString.clone()
 fun main() {
 
     for (i in 1..24) {
@@ -14,29 +13,30 @@ fun main() {
     }
     while (ants.size > 0) {
         time++
-        updateAnt()
-        updateOutput()
+        updateAnts()
+        showAnts()
     }
     println(time)
     println(collisions)
 }
 
-fun updateAnt() {
+fun updateAnts() {
     val willBeRemovedList = mutableListOf<Ant>()
-    outputString = Array(100) { _ -> "." }
+    outputString = Array(100) { "." }
     for ((i, ant) in ants.withIndex()) {
         val newPosition = if (ant.facingRight) ant.position + 1 else ant.position - 1
         if (newPosition in 1..999) {
             ants[i] = Ant(newPosition, ant.facingRight)
-            outputString[(newPosition / 10).toInt()] = if (ant.facingRight) ">" else "<"
+            outputString[(newPosition / 10)] = if (ant.facingRight) ">" else "<"
         } else {
             willBeRemovedList.add(ants[i])
         }
         for ((j, otherAnt) in ants.withIndex()) {
-            if (newPosition == otherAnt.position && (ant.facingRight != otherAnt.facingRight)) {
+            val newOtherAntPosition = if (otherAnt.facingRight) otherAnt.position + 1 else otherAnt.position - 1
+            if ((newPosition in arrayOf(newOtherAntPosition, otherAnt.position)) && (ant.facingRight != otherAnt.facingRight)) {
                 ants[j] = Ant(otherAnt.position, !otherAnt.facingRight)
                 ants[i] = Ant(ant.position, !ant.facingRight)
-                outputString[ant.position / 10] = "X"
+                outputString[newPosition / 10] = "X"
                 collisions++
             }
         }
@@ -44,7 +44,7 @@ fun updateAnt() {
     ants.removeAll { willBeRemovedList.contains(it) }
 }
 
-fun updateOutput() {
+fun showAnts() {
     for (char in outputString) {
         print(char)
     }
